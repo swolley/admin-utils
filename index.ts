af40d449b61150167be8ce41e54ed71b9a8eee70
swolley <myentity@inwind.it>
2022-02-17 23:29:55 +0100

    tentativo pulizia index

--- index.ts
+++ index.ts
@@ -1,27 +1,14 @@
 'use strict';
 
-export interface BaseAdminJQuery {
-	swipe(options: any): any;
-}
-
-export interface BaseAdminWindow {
-	Notify: Notifications;
-	jQuery: JQueryStatic;
-	$: JQueryStatic;
-	Translator: Translator.BazingaTranslator;
-	moment: Function;
-	datatablesTranslations: Object;
-}
-
 declare global {
-	interface JQuery extends BaseAdminJQuery {
+	interface JQuery {
 		swipe(options: any): any;
 	}
-	interface Window extends BaseAdminWindow {
+	interface Window {
 		Notify: Notifications;
 		jQuery: JQueryStatic;
 		$: JQueryStatic;
-		Translator: Translator.BazingaTranslator;
+		// Translator: Translator.BazingaTranslator;
 		moment: Function;
 		datatablesTranslations: Object;
 	}
@@ -33,16 +20,16 @@ declare global {
 import $ from 'jquery';
 window.$ = window.jQuery = $;
 import 'bootstrap';
-import Translator from 'bazinga-translator';
+// import Translator from 'bazinga-translator';
 import { Notifications } from './ts/Notifications';
 import moment from 'moment';
 //#endregion
 
 //jquery overrides
-$.ajaxSetup({ cache: false, timeout: 5000 });
-$.support.transition = false;	//disable jquery transitions
-$.fx.off = true;	//disable jquery animations
+// $.ajaxSetup({ cache: false, timeout: 5000 });
+// $.support.transition = false;	//disable jquery transitions
+// $.fx.off = true;	//disable jquery animations
 
-window.Translator = Translator;
+// window.Translator = Translator;
 window.Notify = new Notifications();
 window.moment = moment;
